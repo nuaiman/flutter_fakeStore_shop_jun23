49c4d92fdda2e8c5949896b93b92f8fa76e97e2f
unknown <ashiqnuaiman@gmail.com>
2023-06-19 19:59:02 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:fake_store_shop_app/features/home/view/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Store App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
